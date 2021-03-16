6b79ebb4e8ff4c0f5586a22343e70aa8f35b968d
markgordon22 <markgordon97@gmail.com>
2021-03-16 23:02:43 +0000

    buttons variable to target buttons when they get clicked

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,5 @@ var elements = document.getElementsByClassName("information")
 
 for(let i=0; i<elements.length; i++) {
 elements[i].style.display="none"}
+
+var buttons = document.getElementsByClassName("informationbutton")
