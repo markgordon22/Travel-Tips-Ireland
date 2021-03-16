7c23fec385ed6cb44632226fdf053d1ce66edf8f
markgordon22 <markgordon97@gmail.com>
2021-03-16 14:12:59 +0000

    if statement above doesnt work then setzoom to 17 - equally nicer zoom

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -128,7 +128,12 @@ function initMap() {
 
       if (place.geometry.viewport) {
           map.fitBounds(place.geometry.viewport);
-        }  
+      }
+        else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(17);
+        }
+    
 
 
 
