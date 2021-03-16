f648c7aef67de54e7f08845f8c0d5cfe68bafb37
markgordon22 <markgordon97@gmail.com>
2021-03-16 23:00:59 +0000

    for loop looping buttons so that it wont display text

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,5 @@
 // target button on ourtips page so when user clicks button the text will appear // 
 var elements = document.getElementsByClassName("information")
+
+for(let i=0; i<elements.length; i++) {
+elements[i].style.display="none"}
