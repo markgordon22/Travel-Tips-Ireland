15285a45599de8fbc3cec37338d1b0ed90447da0
markgordon22 <markgordon97@gmail.com>
2021-03-17 21:29:38 +0000

    deleted button as it was encountering to many bugs with responsiveness

--- assets/js/index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("hello-button").addEventListener("click", function(e){
-  e.preventDefault();
-  
-  document.getElementsByClassName("button-text")[0].innerHTML="Looking for a holiday somewhere in Ireland? Let us help you!";
-});
