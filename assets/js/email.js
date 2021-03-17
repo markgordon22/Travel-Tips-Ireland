5ee981ceab34201daff1d524d17d533a6addaabe
markgordon22 <markgordon97@gmail.com>
2021-03-17 15:50:11 +0000

    thsi will alert user with message if email was not sent

--- assets/js/email.js
+++ assets/js/email.js
@@ -17,6 +17,11 @@ emailjs.send("service_4b99rtc","template_nllrsno",params)
     function(response){
             alert("Your email has been sent, we will be in touch as soon as possible");
 
-},
+}
+
+function(error) {
+            alert("Your email was not sent, please try again");
+            //window.location.reload(true);
+        }
 
 }
