2f6598cbf0246401b40f302e358aed273829a8af
markgordon22 <markgordon97@gmail.com>
2021-03-17 15:23:54 +0000

    variables to get users infomation from the form

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,14 @@
 //email was made with the help of emailjs//
 function sendMail() {
-
+var params = {
+        from_name: "Travel Tips Ireland",
+        first_name: document.getElementById("fname").value,
+        last_name: document.getElementById("lname").value,
+        from: "markgordon97@gmail.com",
+        phone_number: document.getElementById("telephone-number").value,
+        arrival: document.getElementById("datearrival").value,
+        to: document.getElementById("email-address").value,
+        message: document.getElementById("msg").value
+    };
 
 }
