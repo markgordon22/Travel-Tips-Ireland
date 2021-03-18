91ab71c8ab968babe9b563549c571c5926791811
markgordon22 <markgordon97@gmail.com>
2021-03-18 05:45:12 +0000

    inserted comment of what will alert if email unsuccessful

--- assets/js/email.js
+++ assets/js/email.js
@@ -19,7 +19,7 @@ emailjs.send("service_4b99rtc","template_nllrsno",params)
             alert("Your email has been sent, we will be in touch as soon as possible");
 
 },
-
+        // Alert not sent if email was unsuccessful
     function(error) {
             alert("Your email was not sent, please try again");
             window.location.reload(true);
