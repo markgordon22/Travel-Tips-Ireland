3200e764fb26c295ca37e835d1dd67350c190002
markgordon22 <markgordon97@gmail.com>
2021-03-19 09:48:20 +0000

    js code fix - put it into jshint to spot errors

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,22 @@
-// target button on ourtips page so when user clicks button the text will appear // 
-var elements = document.getElementsByClassName("information")
+var elements = document.getElementsByClassName("information");
 
 for(let i=0; i<elements.length; i++) {
-elements[i].style.display="none"}
+elements[i].style.display="none";}
 
-var buttons = document.getElementsByClassName("informationbutton")
+var buttons = document.getElementsByClassName("informationbutton");
 
 for (let b = 0; b<buttons.length; b++) {
 buttons[b].addEventListener("click", function(e){
-e.preventDefault()
+e.preventDefault();
 
-var informationelement = buttons[b].previousElementSibling
+var informationelement = buttons[b].previousElementSibling;
 if(informationelement.style.display === "none")
     {
-    informationelement.style.display = "block"
+    informationelement.style.display = "block";
     }
     else {
-    informationelement.style.display = "none"
+    informationelement.style.display = "none";
     }
 
-})
+});
 }
