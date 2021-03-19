ecd95f0f1b8e0ae42e771f753a99ea91097875c1
markgordon22 <markgordon97@gmail.com>
2021-03-19 03:28:33 +0000

    replaced q with b - b for button reads better

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,11 +6,11 @@ elements[i].style.display="none"}
 
 var buttons = document.getElementsByClassName("informationbutton")
 
-for (let q = 0; q<buttons.length; q++) {
-buttons[q].addEventListener("click", function(e){
+for (let b = 0; b<buttons.length; b++) {
+buttons[b].addEventListener("click", function(e){
 e.preventDefault()
 
-var informationelement = buttons[q].previousElementSibling
+var informationelement = buttons[b].previousElementSibling
 if(informationelement.style.display === "none")
     {
     informationelement.style.display = "block"
